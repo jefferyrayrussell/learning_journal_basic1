803c93a45548a243025b99e143caff41a0faf366
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-25 19:42:36 -0700

    ready for Heroku

--- learning_journal_basic1/routes.py
+++ learning_journal_basic1/routes.py
@@ -1,3 +1,6 @@
 def includeme(config):
-    config.add_static_view('static', 'static', chache_max_age=3600)
-    config.add_route('home', ' / ')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('detail', '/{id:\d+}')
+    config.add_route('create', '/create')
+    config.add_route('update', '/{id:\d+}/edit')
--- learning_journal_basic1/views.py
+++ learning_journal_basic1/views.py
@@ -1,31 +1,36 @@
 from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
 HERE = os.path.dirname(__file__)
 
 
+@view_config(route_name="home", renderer="templates/home.html")
 def list_view(request):
-    imported_text = open(os.path.join(HERE, '/templates/home.html')).read()
+    imported_text = open(os.path.join(HERE, './templates/home.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name="detail", renderer="templates/detail.html")
 def detail_view(request):
-    imported_text = open(os.path.join(HERE, '/templates/detail.html')).read()
+    imported_text = open(os.path.join(HERE, './templates/detail.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name="create", renderer="templates/create.html")
 def create_view(request):
-    imported_text = open(os.path.join(HERE, '/templates/new.html')).read()
+    imported_text = open(os.path.join(HERE, './templates/new.html')).read()
     return Response(imported_text)
 
 
+@view_config(route_name="update", renderer="templates/edit.html")
 def update_view(request):
-    imported_text = open(os.path.join(HERE, '/templates/edit.html')).read()
+    imported_text = open(os.path.join(HERE, './templates/edit.html')).read()
     return Response(imported_text)
 
 
 def includeme(config):
-    config.add_view(list_view, route_name='list_view')
-    config.add_view(detail_view, route_name='detail_view')
-    config.add_view(create_view, route_name='create_view')
-    config.add_view(update_view, route_name='update_view')
+    config.add_view(list_view, route_name='home')
+    config.add_view(detail_view, route_name='detail')
+    config.add_view(create_view, route_name='create')
+    config.add_view(update_view, route_name='update')
