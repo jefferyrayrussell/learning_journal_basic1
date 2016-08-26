63fcbec31a213604371e7c936a1886af0b35c179
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-25 18:18:47 -0700

    initial completion of step 1 assignment

--- learning_journal_basic1/views.py
+++ learning_journal_basic1/views.py
@@ -4,10 +4,28 @@ import os
 HERE = os.path.dirname(__file__)
 
 
-def home_page(request):
-    imported_text = open(os.path.join(HERE, 'sample.txt')).read()
+def list_view(request):
+    imported_text = open(os.path.join(HERE, '/templates/home.html')).read()
+    return Response(imported_text)
+
+
+def detail_view(request):
+    imported_text = open(os.path.join(HERE, '/templates/detail.html')).read()
+    return Response(imported_text)
+
+
+def create_view(request):
+    imported_text = open(os.path.join(HERE, '/templates/new.html')).read()
+    return Response(imported_text)
+
+
+def update_view(request):
+    imported_text = open(os.path.join(HERE, '/templates/edit.html')).read()
     return Response(imported_text)
 
 
 def includeme(config):
-    config.add_view(home_page, route_name='home')
+    config.add_view(list_view, route_name='list_view')
+    config.add_view(detail_view, route_name='detail_view')
+    config.add_view(create_view, route_name='create_view')
+    config.add_view(update_view, route_name='update_view')
