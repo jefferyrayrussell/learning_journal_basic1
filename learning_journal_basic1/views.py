8403d86c0f34b4026c289976243c0b8d3ccdad45
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-28 17:20:43 -0700

    final troubleshooting before testing

--- learning_journal_basic1/views.py
+++ learning_journal_basic1/views.py
@@ -3,10 +3,11 @@
 from pyramid.view import view_config
 
 ENTRIES = [
-    {'title': 'LJ - Test 1', 'creation_date': '08.24.16', 'id': 1, 'body': 'Test text entry 1'},
-    {'title': 'LJ - Test 2', 'creation_date': '08.25.16', 'id': 2, 'body': 'Test text entry 2'},
-    {'title': 'LJ - Test 3', 'creation_date': '08.26.16', 'id': 3, 'body': 'Test text entry 3'},
-    {'title': 'LJ - Test 4', 'creation_date': '08.27.16', 'id': 4, 'body': 'Test text entry 4'},
+    {'title': ' Pyramid, Herokue, DeQue', 'creation_date': '08.22.16', 'id': 1, 'body': 'Today I learned about Pyramid and Heroku.  Zack and I worked on the DeQue data structure.  The best learning of the day was the discussion around testing in code review.  I think one is always a little more attentive when it is your code being reviewed!  The conversation around testing was very helpful.  My take aways were:  think carefully about what you are testing; test not for just positive results and what is expected; test a variety of different types of parameters:  None, long list, 1 item, empty list; tests should be numerous and well thought out.'},
+    {'title': ' Jinja2, Binary Heap', 'creation_date': '08.23.16', 'id': 2, 'body': 'Today I learned about Pyramid Templates and Front-end Testing.  Zack and I worked on the new  data structure.   We spent some time with Will and another team focusing on testing.  We both feel that we need to get much more proficient in this area.  I need how to better use fixtures and the other resources available.  The progress on the learning journal is coming very slowly. I am afraid much of it is going to be left for the weekend.   I was pleased to have the project – election app - I pitched chosen by three other classmates.  It will be great to work with Jeff, Justin, and Crystal.'},
+    {'title': ' Alchemy, Priority Queue', 'creation_date': '08.24.16', 'id': 3, 'body': 'Today I learned about the Priority Queue data structure and the Alchemy Scaffolding.   Each day this week I feel like I am taking one step forward and two steps back in terms of keeping up with assignments.  I am learning, but the pace is brutal.  Thankfully the weekend is near and hopefully I can catch up.  Zack and I continued working on the binary heap and focused on testing. I am really looking forward to project week.'},
+    {'title': ' PostgreSQL, Graph', 'creation_date': '08.25.16', 'id': 3, 'body': 'Today I learned about Graph Data Structures and PostgreSQL and environment variables in Python. Zach and I got a good start on the Priority Queue assignment and Jeff and I worked on Step1 of the Learning Journal assignment. It was a good day; two steps forward and only two and a half steps back.'},
+    {'title': ' Career, Catch Up', 'creation_date': '08.26.16', 'id': 3, 'body': 'Today I was a catch up Friday.  Zach and I worked on the graph data structure.  We were methodical in sketching, pseudo-code, testing, and writing our functions.  It was valuable time spent.  So also was the morning professional development session.  There was a mix of new information and timely reminders about strategies for pitch, profile statement, and tech resumes.'},
 ]
 
 
