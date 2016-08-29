d3fded81a80002e62cc78920800ac0ba0d056ec2
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-28 17:43:51 -0700

    initial testing setup including tox.ini

--- learning_journal_basic1/tests.py
+++ learning_journal_basic1/tests.py
@@ -1,29 +1,41 @@
-import unittest
+import pytest
 
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'learning_journal_basic1')
+def test_list_view():
+    """Test that entries are properly entered into list view."""
+    from .views import list_view
+    request = testing.DummyRequest()
+    info = list_view(request)
+    assert "entries" in info
 
 
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
-        from learning_journal_basic1 import main
-        app = main({})
-        from webtest import TestApp
-        self.testapp = TestApp(app)
+def test_detail_view():
+    """Test that correct entry appears in detail view."""
+    request = testing.DummyRequest()
+    request.id = 1
 
-    def test_root(self):
-        res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+
+
+@pytest.fixture()
+def testapp():
+    from learning_journal_basic1 import main
+    app = main({})
+    from webtest import testapp
+    return TestApp(app)
+
+
+def test_layout_root(testapp):
+    response = testapp.get('/', status=200)
+    assert b'Jinja2, Binary Heap' in response.body
+
+
+def test_root_contents(testapp):
+    """Tests that the contents of the root page contin as many articles as journal entries."""
+    from .views import ENTRIES
+    response = testapp.get('/', status=200)
+    html = response.html
+    assert len(ENTRIES) == len(html.finalAll("article"))
+
+    
