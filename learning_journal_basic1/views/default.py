cfdbeab92f8bc39bd6efd9efaf81cdffe47d3f99
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-29 15:37:27 -0700

    changes for sqlite

--- learning_journal_basic1/views/default.py
+++ learning_journal_basic1/views/default.py
@@ -6,11 +6,11 @@ from sqlalchemy.exc import DBAPIError
 from ..models import MyModel
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='../templates/list.jinja2')
 def my_view(request):
     try:
         query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
+        one = query.filter(MyModel.title == 'one').first()
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
     return {'one': one, 'project': 'jeff_learning_journal'}
